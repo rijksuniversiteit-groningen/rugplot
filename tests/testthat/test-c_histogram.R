6164832a05828364b1631e72944a53881f0e635d
Venustiano <v.soancatl.aguilar@rug.nl>
2022-10-24 21:44:38 +0200

    fix json

--- tests/testthat/test-c_histogram.R
+++ tests/testthat/test-c_histogram.R
@@ -1,8 +1,9 @@
 test_that("Validating columns", {
-  lp <- validate_json_file("params/mpg_params.json")
+  js <- "params/violinparams_test.json"
+  lp <- validate_json_file(js)
   expect_type(lp,"list")
 
-  validate_parameters("params/mpg_params.json","histogram_schema.json")
+  validate_parameters(js,"histogram_schema.json")
   # expect_error(histogram("params/hist_incorrect-col_id.json"),
   #              "' length ' must be a column in data/iris.csv")
 
